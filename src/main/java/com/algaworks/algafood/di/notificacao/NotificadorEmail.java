15fa4c5f3912ca4e00a55b2fbd874a12f9e617ca
Kauê Oliveira <kso13@hotmail.com>
2023-08-27 22:03:26 -0300

    02.12 - Injetando dependencias beans spring

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -5,10 +5,15 @@ import com.algaworks.algafood.di.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NotificadorEmail {
+public class NotificadorEmail implements Notificador {
 
+	public NotificadorEmail() {
+		System.out.println("Notificador email" + this);
+	}
+
+	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
+		System.out.printf("Notificando %s através do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 }
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,13 +1,18 @@
 package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NotificadorEmail;
+import com.algaworks.algafood.di.notificacao.Notificador;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
-	private NotificadorEmail notificador;
+	private final Notificador notificador;
+
+	public AtivacaoClienteService(Notificador notificador) {
+		this.notificador = notificador;
+		System.out.println(this.getClass().getSimpleName() + " " + notificador);
+	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
