01c24cf40a9b1a64eb3b1d7b9fcbf81eab18533b
Kauê Oliveira <kso13@hotmail.com>
2023-08-28 00:36:52 -0300

    02.19 - Desambiguação de beans com anotação customizada

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
@@ -3,7 +3,6 @@ package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.TipoDoNotificador;
 import com.algaworks.algafood.di.modelo.Cliente;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @TipoDoNotificador(NivelUrgencia.URGENTE)
