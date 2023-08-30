9dfa06b0fddb870d2d8ed941628748532c932825
Kauê Oliveira <kso13@hotmail.com>
2023-08-30 06:58:31 -0300

    03.04 - Mapeando entidades com JPA

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,47 @@
+package com.algaworks.algafood.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tab_cozinhas")
+public class Cozinha {
+
+    @Id
+    private Long id;
+    @Column(name = "nome_cozinha")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cozinha cozinha = (Cozinha) o;
+        return Objects.equals(id, cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
