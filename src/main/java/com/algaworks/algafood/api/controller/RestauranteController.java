350f8a2b3c5c6772079c9db964debd9e827e9b63
Kauê Oliveira <kso13@hotmail.com>
2023-09-05 05:35:38 -0300

    04.30 - Modelando e implementando a inclusão de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +18,8 @@ public class RestauranteController {
     @Autowired
     private RestauranteRepository restauranteRepository;
 
-//    private CadastroRestauranteService cadastroService;
+    @Autowired
+    private CadastroRestauranteService cadastroService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -30,5 +33,17 @@ public class RestauranteController {
         if (restaurante != null)
             return ResponseEntity.ok(restaurante);
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+        try {
+            restaurante = cadastroService.salvar(restaurante);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(restaurante);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
