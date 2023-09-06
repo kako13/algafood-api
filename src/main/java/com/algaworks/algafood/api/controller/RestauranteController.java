343d7218e3467a4bea556e4856ea69f349722641
Kauê Oliveira <kso13@hotmail.com>
2023-09-06 06:58:42 -0300

    04.33 - Analisando solução para atualização parcial de recursos com PATCH

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -61,5 +62,20 @@ public class RestauranteController {
         } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> atualizarParcial(@PathVariable Long id, @RequestBody Map<String, Object> campos) {
+        Restaurante restauranteAtual = restauranteRepository.buscar(id);
+        if(restauranteAtual == null)
+            return ResponseEntity.notFound().build();
+        merge(campos, restauranteAtual);
+        return atualizar(id, restauranteAtual);
+    }
+
+    private static void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
+        camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            System.out.println(nomePropriedade + " " + valorPropriedade);
+        });
     }
 }
