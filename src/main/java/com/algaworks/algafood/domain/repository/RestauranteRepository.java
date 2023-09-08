ce3d653a556aa4addc5e333229c1bbe9d3895b43
Kauê Oliveira <kso13@hotmail.com>
2023-09-08 03:16:03 -0300

    05.10 - Externalizando consultas JPQL para um arquivo XML

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,7 +2,6 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
