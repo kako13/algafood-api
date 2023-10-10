dc4c1807d4725454df466fed30683842ff35c39d
Kauê Oliveira <kso13@hotmail.com>
2023-10-10 06:10:44 -0300

    10.09 - Validando o corpo da resposta HTTP

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -29,5 +31,21 @@ class CadastroCozinhaIT {
                 .get()
         .then()
                 .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveConterQuatroCozinhas_QuandoConsultarCozinhas() {
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+        .when()
+                .get()
+        .then()
+                .body("", hasSize(4))
+                .body("nome", hasItems("Brasileira", "Tailandesa"));
     }
 }
