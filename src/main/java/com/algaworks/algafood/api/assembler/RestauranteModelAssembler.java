3581ce84cc4b64f000a7b4873b1b7ce252baa65f
Kauê Oliveira <kso13@hotmail.com>
2023-10-30 08:21:15 -0300

    11.12 - Refatorando e criando um assembler de DTO

---
+++ src/main/java/com/algaworks/algafood/api/assembler/RestauranteModelAssembler.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.api.assembler;
+
+import com.algaworks.algafood.api.model.CozinhaModel;
+import com.algaworks.algafood.api.model.RestauranteModel;
+import com.algaworks.algafood.domain.model.Restaurante;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class RestauranteModelAssembler {
+    public RestauranteModel toModel(Restaurante restaurante) {
+        CozinhaModel cozinhaModel = new CozinhaModel();
+        cozinhaModel.setId(restaurante.getCozinha().getId());
+        cozinhaModel.setNome(restaurante.getCozinha().getNome());
+
+        RestauranteModel restauranteModel = new RestauranteModel();
+        restauranteModel.setId(restaurante.getId());
+        restauranteModel.setNome(restaurante.getNome());
+        restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
+        restauranteModel.setCozinha(cozinhaModel);
+        return restauranteModel;
+    }
+
+    public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
+        return restaurantes.stream()
+                .map(e -> toModel(e))
+                .collect(Collectors.toList());
+    }
+}
